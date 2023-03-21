a1122e856cad32649a8529940dae3b5fc5c7a915
Sergei Anisimov <serge.anisimoff@gmail.com>
2023-03-21 19:28:03 +0300

    Initial Commit

---
+++ JSONPlaceholderUsers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONPlaceholderUsers
+//
+//  Created by Alex Murphy on 21.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
